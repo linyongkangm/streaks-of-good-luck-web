5f3ff52cf9a888bbce149061a8b242b18c9f4fb8
linyongkangm <linyongkangm@foxmail.com>
2026-02-03 15:50:39 +0800

    feat: add SecuritiesMetadataQuotes component and integrate it into SecuritiesMetadata

--- app/components/SecuritiesMetadata/SecuritiesMetadata.tsx
+++ app/components/SecuritiesMetadata/SecuritiesMetadata.tsx
@@ -3,13 +3,13 @@
 import { useState, useEffect } from 'react'
 import type { info__stock_company } from '@/types'
 import SecuritiesMetadataCompanies from './SecuritiesMetadataCompanies'
-
+import SecuritiesMetadataQuotes from './SecuritiesMetadataQuotes'
 export default function SecuritiesMetadata() {
   return (
     <div className="p-6 max-w-[1600px] mx-auto">
       <div className="bg-white rounded-xl shadow-lg p-6 mb-6">
         <SecuritiesMetadataCompanies></SecuritiesMetadataCompanies>
-        
+        <SecuritiesMetadataQuotes></SecuritiesMetadataQuotes>
       </div>
     </div>
   )
---
+++ app/components/SecuritiesMetadata/SecuritiesMetadataQuotes.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import type { info__stock_company } from '@/types'
+
+export default function SecuritiesMetadataQuotes() {
+  return (
+    <div className="mx-auto">
+      <div className="w-full">
+        
+      </div>
+    </div>
+  )
+}
